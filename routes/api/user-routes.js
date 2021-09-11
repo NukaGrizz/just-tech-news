425ec24dbef9166fd5841d2bb417291070dd85ae
NukaGrizz <nukagrizz@gmail.com>
2021-09-10 23:18:50 -0400

    add file structures and routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//Get /api/users
+router.get('/', (req, res) => {});
+
+//Get /api/users/1
+router.get('/:id', (req, res) => {});
+
+//POST /api/users
+router.post('/', (req, res) => {});
